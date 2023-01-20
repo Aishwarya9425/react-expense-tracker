afc7f7ebc8e2af65a6a5bd2622b5f5594854976e
Aishwarya Janakiraman <aishwarya9425@gmail.com>
2023-01-20 14:26:59 +0530

    redux setup

--- src/index.js
+++ src/index.js
@@ -2,10 +2,14 @@ import "./global.css";
 import { App } from "./App";
 import React, { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import { store } from "./store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </StrictMode>
 );
