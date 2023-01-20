f0b704c4ed9626300857333785612c6ee4301deb
Aishwarya Janakiraman <aishwarya9425@gmail.com>
2023-01-20 12:18:03 +0530

    Initial commit

---
+++ src/components/ExpenseInput/ExpenseInput.jsx
@@ -0,0 +1,33 @@
+import s from "./style.module.css";
+
+export function ExpenseInput(props) {
+  return (
+    <form>
+      <div className="row justify-content-center">
+        <div className="col-12 col-sm-5 col-md-4 col-lg-4 mb-2">
+          <input
+            type="text"
+            className="form-control"
+            placeholder='Ex : "Apple"'
+            name="name"
+          />
+        </div>
+        <div className="col-12 col-sm-2 col-md-4 col-lg-4 mb-2">
+          <input
+            type="number"
+            step="0.01"
+            className="form-control"
+            placeholder="Ex: 3.99"
+            name="price"
+          />
+        </div>
+
+        <div className="col-12 col-sm-2 col-md-4 col-lg-4 mb-2">
+          <button type="submit" className={`btn btn-primary ${s.btn}`}>
+            Add
+          </button>
+        </div>
+      </div>
+    </form>
+  );
+}
