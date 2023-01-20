709520362825ce37c193f5ce91dfa1702af33bb7
Aishwarya Janakiraman <aishwarya9425@gmail.com>
2023-01-20 19:46:22 +0530

    income input container

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import { ExpenseInput } from "containers/ExpenseInput/ExpenseInput";
 import s from "./style.module.css";
 import { ExpenseList } from "containers/ExpenseList/ExpenseList";
+import { IncomeInput } from "containers/IncomeInput/IncomeInput";
 
 export function App() {
   return (
     <div className={s.main_container}>
       <div className={`row ${s.header}`}>
         <div className={`col-3`}>Logo</div>
-        <div className={`col-9 ${s.income_input}`}>Income input</div>
+        <div className={`col-9 ${s.income_input}`}>
+          <IncomeInput />
+        </div>
       </div>
       <div className={`row ${s.workspace}`}>
         <div className={`col-12  ${s.expense_input}`}>
--- src/store/expense/expense-slice.js
+++ src/store/expense/expense-slice.js
@@ -1,16 +1,8 @@
 const { createSlice } = require("@reduxjs/toolkit");
-
-// {
-//     "type": "expenseSlice/addExpenseAction",
-//     "payload": {
-//         "price": "444",
-//         "name": "sddasd"
-//     }
-// }
-
 export const expenseSlice = createSlice({
   name: "expenseSlice",
   initialState: {
+    income: 1000,
     expenseList: [],
   },
   reducers: {
@@ -20,6 +12,11 @@ export const expenseSlice = createSlice({
       //currentSlice.expenseList.push({ name: "Watermelon", price: 33 });
       currentSlice.expenseList.push(action.payload);
     },
+    setIncomeAction: (currentSlice, action) => {
+      console.log("setIncomeAction func is called");
+      console.log("action", action);
+      currentSlice.income = action.payload;
+    },
   },
 });
-export const { addExpenseAction } = expenseSlice.actions;
+export const { addExpenseAction,setIncomeAction } = expenseSlice.actions;
